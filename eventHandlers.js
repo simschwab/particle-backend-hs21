8843a05fbab3803cd8f645914869851aa09be43a
Simon Schwab <sim.schwab@me.com>
2021-11-07 18:38:20 +0100

    added events

--- eventHandlers.js
+++ eventHandlers.js
@@ -4,7 +4,12 @@ exports.sendEvent = null;
 
 exports.registerEventHandlers = function (source) {
     source.addEventListener('MyEvent', handleMyEvent);
-    // Register more event handlers here
+    source.addEventListener('motion-detected', handleMotionDetection);
+    source.addEventListener('training-started', handleTrainingStarted);
+    source.addEventListener('traning-stopped', handleTrainingStopped);
+    source.addEventListener('Cadence', handleCadence);
+
+
 }
 
 function handleMyEvent(event) {
@@ -25,9 +30,93 @@ function handleMyEvent(event) {
         // TODO: do something meaningful with the data
 
         // Log the event in the database
-        // logger.logOne("MyDB", "MyEvent", data);
+        logger.logOne("MyDB", "MyEvent", data);
 
         // send data to all connected clients
+        exports.sendEvent(data);
+    } catch (error) {
+        console.log("Could not handle event: " + JSON.stringify(event) + "\n");
+        console.log(error)
+    }
+}
+
+function handleMotionDetection(event) {
+
+    var data = {
+        eventName: event.type,
+        eventData: JSON.parse(event.data).data, // the value of the event
+        deviceId: JSON.parse(event.data).coreid,
+        timestamp: JSON.parse(event.data).published_at
+    };
+
+    try {        
+        data.myMessage = "Motion Detected";
+
+        logger.logOne("MyDB", "MotionDetected", data);
+
+        exports.sendEvent(data);
+    } catch (error) {
+        console.log("Could not handle event: " + JSON.stringify(event) + "\n");
+        console.log(error)
+    }
+}
+
+function handleTrainingStarted(event) {
+
+    var data = {
+        eventName: event.type,
+        eventData: JSON.parse(event.data).data,
+        deviceId: JSON.parse(event.data).coreid,
+        timestamp: JSON.parse(event.data).published_at
+    };
+
+    try {        
+        data.myMessage = "Training started";
+
+        logger.logOne("MyDB", "TrainingStarted", data);
+
+        exports.sendEvent(data);
+    } catch (error) {
+        console.log("Could not handle event: " + JSON.stringify(event) + "\n");
+        console.log(error)
+    }
+}
+
+function handleTrainingStopped(event) {
+
+    var data = {
+        eventName: event.type,
+        eventData: JSON.parse(event.data).data,
+        deviceId: JSON.parse(event.data).coreid,
+        timestamp: JSON.parse(event.data).published_at
+    };
+
+    try {        
+        data.myMessage = "Training stopped";
+
+        logger.logOne("MyDB", "TrainingStopped", data);
+
+        exports.sendEvent(data);
+    } catch (error) {
+        console.log("Could not handle event: " + JSON.stringify(event) + "\n");
+        console.log(error)
+    }
+}
+
+function handleCadence(event) {
+
+    var data = {
+        eventName: event.type,
+        eventData: JSON.parse(event.data).data,
+        deviceId: JSON.parse(event.data).coreid,
+        timestamp: JSON.parse(event.data).published_at
+    };
+
+    try {        
+        data.myMessage = "Cadence";
+
+        logger.logOne("MyDB", "Cadence", data);
+
         exports.sendEvent(data);
     } catch (error) {
         console.log("Could not handle event: " + JSON.stringify(event) + "\n");
